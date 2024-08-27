ebd9cbb80458d83b704d3ad67d942e0251d3088b
vyuba <preyealexander731@gmail.com>
2024-08-27 15:09:04 +0100

    animated a loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,48 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+import { useEffect } from 'react';
+import { gsap } from 'gsap';
 
-// import Loader from "./components/Loader"
-import Footer from "./components/Footer"
-import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
-
+import Loader from "./components/Loader";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
+
+  useEffect(() => {
+    const tl = gsap.timeline();
+
+    tl.to('.loader', {
+      opacity: 0,
+      duration: 2, // Duration of fade-out
+      delay: 5,   // Stay visible for 20 seconds before fading out
+      onComplete: () => {
+        y: '-10000'
+      }
+    });
+
+    tl.from('.app-content', {
+      opacity: 1,
+      duration: 2,
+      ease: 'power3.out'
+    });
+  }, []);
 
   return (
     <>
       <div>
-        {/* <Loader/> */}
-        <Navbar/>
-        <div className="pt-32">
-        <Home/>
-        <Footer/>
+        <div className="loader">
+          <Loader />
+        </div>
+        <div className="app-content">
+          <Navbar />
+          <div className="pt-32">
+            <Home />
+            <Footer />
+          </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,12 +1,32 @@
 // import React from 'react'
-
+import { gsap } from 'gsap';
+import { useGSAP } from '@gsap/react';
 function Loader() {
+  gsap.registerPlugin(useGSAP)
+  useGSAP(
+    ()=>{
+      gsap.from(
+        '.progress',
+        {
+          width: 0,
+          duration: 2
+        }
+      )
+      gsap.to(
+        '.progress',
+        {
+          width: '100%',
+          duration: 4
+        }
+      )
+    }
+  )
   return (
-    <div className="Loader inset-0 fixed w-screen h-screen bg-[#F8F9FA] grid place-items-center">
+    <div className="Loader z-[100000] inset-0 fixed w-screen h-screen bg-[#F8F9FA] grid place-items-center">
         <div className="w-fit flex flex-col gap-2">
             <h1 className="uppercase text-sm md:text-xl">welcome to nexusweb3</h1>
-            <div className="w-full h-1 bg-white">
-                <div className="w-2/5 bg-[#FF2B2B] h-full"></div>
+            <div className="w-full h-1 bg-white progress-container">
+                <div className=" bg-[#FF2B2B] h-full progress"></div>
             </div>
         </div>
     </div>
